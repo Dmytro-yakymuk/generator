f1b512bd674393fb29b67d1b2f78d2768e648ba2
Dmytro Yakimuk <dmytroyakimuk@gmail.com>
2021-12-31 16:18:43 +0200

    added work version

---
+++ internal/probability/probability.go
@@ -0,0 +1,380 @@
+package probability
+
+var Heads = map[string]int{
+	// 35 - 1000
+	// 9 - 116
+	"head 1":           3,
+	"irridescent-rare": 5,
+	"head 25":          8,
+	"head 31":          10,
+	"head 24":          12,
+	"head 21":          15,
+	"head 15":          18,
+	"head 11":          20,
+	"head 12":          25,
+
+	// 26 - 884
+	"head 2":  34,
+	"head 3":  34,
+	"head 4":  34,
+	"head 5":  34,
+	"head 6":  34,
+	"head 8":  34,
+	"head 9":  34,
+	"head 10": 34,
+	"head 13": 34,
+	"head 16": 34,
+	"head 17": 34,
+	"head 18": 34,
+	"head 19": 34,
+	"head 20": 34,
+	"head 22": 34,
+	"head 23": 34,
+	"head 26": 34,
+	"head 27": 34,
+	"head 28": 34,
+	"head 29": 34,
+	"head 30": 34,
+	"head 32": 34,
+	"head 33": 34,
+	"head_34": 34,
+	"head_35": 34,
+	"head_36": 34,
+}
+
+var Tshirts = map[string]int{
+	// 28 - 1000
+	// 13 - 325
+	"tshirt 30":        10,
+	"tshirt 29":        12,
+	"goonz-reaper0026": 15,
+	"goonz-tshirt0026": 18,
+	"tshirt 26":        20,
+	"goongang0026":     22,
+	"skull0026":        25,
+	"tshirt 3":         28,
+	"4":                30,
+	"7":                32,
+	"tshirt 25":        35,
+	"tshirt 24":        38,
+	"tshirt 22":        40,
+
+	// 15 - 675
+	"9":                    45,
+	"10":                   45,
+	"11":                   45,
+	"14":                   45,
+	"22":                   45,
+	"black thisrt common":  45,
+	"goonz-graphic0026":    45,
+	"goonz-pokemon0026":    45,
+	"pgc0026":              45,
+	"tshirt2":              45,
+	"tshirt 18":            45,
+	"tshirt 21":            45,
+	"tshirt 23":            45,
+	"tshirt 31":            45,
+	"whiote tshirt common": 45,
+}
+
+var Necklaces = map[string]int{
+	// 13 - 1000
+	"layer 33":             15,
+	"diamond head pendant": 25,
+	"layer 35":             35,
+	"apple watch chain":    45,
+	"color0025-copy":       55,
+	"layer 32":             65,
+	"pg logo":              75,
+	"pg logo copy":         85,
+	"bitcoin gold small":   95,
+	"face logo copy":       105,
+	"face logo":            120,
+	"layer 30":             130,
+	"layer 34":             150,
+}
+
+var Jackets = map[string]int{
+	// 12 - 1000
+	// 3 - 154
+	"layer 26 copy 2": 40,
+	"layer 26":        52,
+	"layer 26 copy 6": 62,
+
+	// 9 - 846
+	"26_0026":          94,
+	"layer 26 copy 3":  94,
+	"layer 26 copy 4":  94,
+	"layer 26 copy 7":  94,
+	"layer 26 copy 9":  94,
+	"layer 26 copy 10": 94,
+	"layer 26 copy 11": 94,
+	"layer 26 copy 12": 94,
+	"layer 26 copy 14": 94,
+}
+
+var Updo string = "punishergoonn00226"
+var Hairs = map[string]int{
+	// 26 - 1000
+	// 18 - 344
+	"flame purple": 3,
+	"flame":        5,
+	"heart":        7,
+	"tiger orange": 10,
+	"tiger red":    12,
+	"leopard":      15,
+	"mary":         17,
+	"skull copy":   20,
+	"skull":        21,
+	"1":            22,
+	"9 copy":       23,
+	"17":           24,
+	"18":           25,
+	"19":           26,
+	"20":           27,
+	"21":           28,
+	"23":           29,
+	"24":           30,
+
+	// 8 - 656
+	"3":                  82,
+	"4":                  82,
+	"8":                  82,
+	"10":                 82,
+	"11":                 82,
+	"12":                 82,
+	"16":                 82,
+	"punishergoonn00226": 82,
+}
+
+var Hats = map[string]int{
+	// 33 - 1000
+	// 26 - 622
+	"helicopter-final":           3,
+	"squiggle hat":               5,
+	"jacko-colour0022":           8,
+	"black cap backward":         10,
+	"flip peak rare":             12,
+	"goonz black hat variant":    15,
+	"black hat graffiti variant": 18,
+	"black hat p goon variant":   20,
+	"paisley0072":                21,
+	"rare pradagoon":             22,
+	"layer 7":                    23,
+	"greencamo0072":              24,
+	"camo0018":                   25,
+	"white":                      26,
+	"red":                        27,
+	"blue copy":                  28,
+	"blue":                       29,
+	"leather0018":                30,
+	"red hat common":             31,
+	"blue hat":                   32,
+	"blue hat common":            33,
+	"blue hat pattern":           34,
+	"pink hat variant":           35,
+	"purple hat":                 36,
+	"purple stripe backward":     37,
+	"purple0018":                 38,
+
+	// 7 - 378
+	"black0018":         57,
+	"black fedora copy": 57,
+	"green hat common":  57,
+	"layer 24":          57,
+	"red0018":           57,
+	"silver geo hart":   57,
+	"snowcamo0072":      57,
+}
+
+var Glasses = map[string]int{
+	// 14 - 1000
+	"heart2_0014 copy":       10,
+	"layer 4 copy":           20,
+	"bitcoin0014":            30,
+	"eth0014":                40,
+	"google-trance0014":      50,
+	"google left 0014":       60,
+	"googly-cross0014":       70,
+	"googly-stoned0014":      80,
+	"layer1copy":             90,
+	"layer2copy":             95,
+	"layer2":                 100,
+	"layer4":                 110,
+	"smiley0014":             120,
+	"rayban-frame-color0014": 125,
+}
+
+var Earrings = map[string]int{
+	// 4 - 1000
+	"DIAMON-rare":    100,
+	"gold bitcoin":   200,
+	"eth black copy": 300,
+	"eth black":      400,
+}
+
+//
+//
+//
+//
+//
+// 2 ////////////////////////////////
+var Tshirts2 = map[string]int{
+	// 14 - 1000
+	"1":               10,
+	"10":              20,
+	"11":              30,
+	"14":              40,
+	"12":              50,
+	"goonz black0026": 60,
+	"13":              70,
+	"5":               80,
+	"2":               90,
+	"6":               95,
+	"7":               100,
+	"3":               110,
+	"8":               120,
+	"9":               125,
+}
+
+//
+//
+//
+//
+//
+// 3 ////////////////////////////////
+var Tshirts3 = map[string]int{
+	// 3 - 1000
+	"weed":       200,
+	"swag2":      300,
+	"red-stripe": 500,
+}
+
+//
+//
+//
+//
+//
+// 4 ////////////////////////////////
+var Tshirts4 = map[string]int{
+	// 1 - 1000
+	"layer 11": 1000,
+}
+
+//
+//
+//
+//
+//
+// 5 ////////////////////////////////
+var Tshirts5 = map[string]int{
+	// 4 - 1000
+	"2_0026": 100,
+	"1_0026": 200,
+	"3_0026": 300,
+	"4_0026": 400,
+}
+
+//
+//
+//
+//
+//
+//
+// 6 ////////////////////////////////
+var Tshirts6 = map[string]int{
+	// 2 - 1000
+	"SUEDE RED":     350,
+	"SUEDE BLACK 2": 650,
+}
+
+var Hairs6 = map[string]int{
+	// 25 - 1000
+	// 18 - 349
+	"flame purple": 4,
+	"flame":        6,
+	"heart":        8,
+	"tiger orange": 10,
+	"tiger red":    13,
+	"leopard":      15,
+	"mary":         18,
+	"skull copy":   20,
+	"skull":        21,
+	"1":            22,
+	"9 copy":       23,
+	"17":           24,
+	"18":           25,
+	"19":           26,
+	"20":           27,
+	"21":           28,
+	"23":           29,
+	"24":           30,
+
+	// 7 - 651
+	"3":  93,
+	"4":  93,
+	"8":  93,
+	"10": 93,
+	"11": 93,
+	"12": 93,
+	"16": 93,
+}
+
+var Hats6 = map[string]int{
+	// 33 - 1000
+	"1_0022":         125,
+	"layer 6":        125,
+	"layer 6 copy":   125,
+	"layer 6 copy 2": 125,
+	"layer 7":        125,
+	"layer 7 copy":   125,
+	"layer 7 copy 2": 125,
+	"layer 7 copy 3": 125,
+}
+
+var Glasses6 = map[string]int{
+	// 12 - 1000
+	"layer 4 copy":       10,
+	"bitcoin0014":        30,
+	"eth0014":            50,
+	"google--trance0014": 60,
+	"google-left0014":    70,
+	"googly--cross0014":  80,
+	"googly--stoned0014": 90,
+	"layer1 copy":        100,
+	"layer 2 copy":       110,
+	"layer 2":            120,
+	"layer 4":            130,
+	"smiley0014":         150,
+}
+
+//
+//
+//
+//
+//
+//
+// 7 ////////////////////////////////
+
+var Tshirts7 = map[string]int{
+	// 2 - 1000
+	"1_0026": 500,
+	"3_0026": 500,
+}
+
+var Hats7 = map[string]int{
+	// 1 - 1000
+	"LAYER 13": 1000,
+}
+
+var Glasses7 = map[string]int{
+	// 8 - 1000 + 400
+	"layer 4 copy":       50,
+	"google--trance0014": 75,
+	"google-left0014":    100,
+	"googly-cross0014":   125,
+	"googy--stoned0014":  150,
+	"layer 1 copty":      155,
+	"layer 4":            165,
+	"smiley0014":         180,
+}
